909da1919707041cc7c579bf078373c539a94ea4
Superman9999 <agnelonunes43@gmail.com>
2024-11-20 21:18:50 -0500

    Update app.py

--- testing/app.py
+++ testing/app.py
@@ -2,5 +2,8 @@ import pytest
 import requests
 
 def test_homepage():
-    response = requests.get('http://web-app-frontend.frontend.svc.cluster.local:5000/')
+    response = requests.get('http://web-app-frontend.frontend.svc.cluster.local/')
     assert response.status_code == 200
+
+def test_example():
+    assert 1 + 1 == 2
